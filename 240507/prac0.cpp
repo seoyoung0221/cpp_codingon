c8073ac29de2e8fe89fca1a9ca5127ef0f62e7c5
seoyoung0221 <jjan000221@gmail.com>
2024-05-07 16:09:35 +0900

    prac0(v.back())

--- 240507/prac0.cpp
+++ 240507/prac0.cpp
@@ -12,28 +12,35 @@ int main()
 {
     vector<string> v;
 
+    //hello 파일 열기
     ifstream read_file("hello.txt");
 
     if (read_file.is_open())
     {
-        string line;
         for (int i = 0; i < 3; i++)
         {
+            //라인 받아오기
+            string line;
             getline(read_file, line);
             v.push_back(line);
         }
     }
+    //hello 파일 닫기
     read_file.close();
 
+    //output 파일 열기
     ofstream write_file("output.txt");
+
+    //output 파일에 문장 적기
     if (write_file.is_open()) {
         for (int i = 0; i < 3; i++)
         {
-            write_file << v[v.size()-1] << "\n";
-            v.pop_back();
+            write_file << v.back() << "\n";
+            v.pop_back(); //마지막 요소 제거
         }
     }
 
+    //output 파일 닫기
     write_file.close();
     
 
