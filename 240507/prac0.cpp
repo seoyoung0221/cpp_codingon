60c0cd83a8a03d035522373c00fb6d90ff075cfe
seoyoung0221 <jjan000221@gmail.com>
2024-05-07 16:03:23 +0900

    prac0(push_back, pop_back)

--- 240507/prac0.cpp
+++ 240507/prac0.cpp
@@ -10,24 +10,31 @@ using namespace std;
 
 int main()
 {
-    vector<string> v(3);
+    vector<string> v;
 
     ifstream read_file("hello.txt");
 
     if (read_file.is_open())
     {
+        string line;
         for (int i = 0; i < 3; i++)
         {
-            getline(read_file, v[i]);
+            getline(read_file, line);
+            v.push_back(line);
         }
     }
     read_file.close();
 
     ofstream write_file("output.txt");
-    for (int i = 0; i < 3; i++)
-    {
-        write_file << v[3 - i - 1] << "\n";
+    if (write_file.is_open()) {
+        for (int i = 0; i < 3; i++)
+        {
+            write_file << v[v.size()-1] << "\n";
+            v.pop_back();
+        }
     }
+
+    write_file.close();
     
 
     return 0;
