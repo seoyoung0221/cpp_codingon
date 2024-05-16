ae610d32aa27a6406e460050a92e864bcadb59d5
seoyoung0221 <jjan000221@gmail.com>
2024-05-16 16:25:34 +0900

    prac2

--- 240516/prac1.cpp
+++ 240516/prac1.cpp
@@ -1,5 +1,6 @@
 //240516
 //prac1 상속 사용해보기
+//prac2 오버라이딩 이해하기
 
 #include <iostream>
 
@@ -7,6 +8,7 @@ using namespace std;
 
 class Shape {
 public :
+	Shape() {};
 	Shape(float width, int lineNum)
 	{
 		this->m_width = width;
@@ -29,6 +31,7 @@ public :
 	Rectangle(float width, float height)
 		: Shape(width, 4)
 	{
+		this->m_width = width;
 		this->m_height = height;
 	};
 
@@ -37,22 +40,43 @@ public :
 		cout << "넓이 : " << this->m_width * this->m_height << "\n";
 	}
 
+	void printInfo() 
+	{
+		cout << "사각형의 넓이는 " << this->m_width * this->m_height << "\n";
+	}
+
 private: 
+	float m_width;
 	float m_height;
 };
 
 class Triangle : public Shape
 {
 public : 
+	/*
 	Triangle(float width, float height)
-		: Shape(width, 3), m_height(height){};
+		: Shape(width, 3), m_height(height), m_width(width){};
+		*/
+
+	Triangle(float width, float height) 
+	{
+		this->m_width = width;
+		this->m_height = height;
+		this->m_lineNum = 3;
+	}
 
 	void area()
 	{
 		cout << "넓이 : " << (this->m_width * this->m_height) / 2 << "\n";
 	}
 
+	void printInfo()
+	{
+		cout << "삼각형의 넓이는 " << this->m_width * this->m_height << "\n";
+	}
+
 private:
+	float m_width;
 	float m_height;
 };
 
@@ -66,7 +90,7 @@ int main() {
 
 	Triangle triangle(tri_width, tri_height);
 	triangle.printInfo();
-	triangle.area();
+	//triangle.area();
 
 	cout << "\n";
 
@@ -78,7 +102,7 @@ int main() {
 
 	Rectangle rectangle(rect_width, rect_height);
 	rectangle.printInfo();
-	rectangle.area();
+	//rectangle.area();
 
 
 	return 0;
