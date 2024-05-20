6d6a5f1aaff3817085a1e000b4ecca6776c8c501
seoyoung0221 <jjan000221@gmail.com>
2024-05-20 17:09:40 +0900

    prac2_virtualFunc

---
+++ 240516/prac2_virtualFunc.cpp
@@ -0,0 +1,76 @@
+//240520
+//가상함수
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+public:
+	virtual void intro() {
+		cout << "사람입니다~" << endl;
+	}
+};
+
+class Student : public Person {
+	string name;
+
+public: 
+	Student(string name) {
+		this->name = name;
+	}
+	void intro() {
+		cout << name << "학생입니다." << endl;
+	}
+	void learn() {
+		cout << "배웁니다." << endl;
+	}
+};
+
+class Teacher : public Person {
+	string name;
+
+public:
+	Teacher(string name) {
+		this->name = name;
+	}
+
+	void intro() {
+		cout << name << "선생입니다." << endl;
+	}
+
+	void teach() {
+		cout << "가르칩니다." << endl;
+	}
+};
+
+int main() {
+	Person* pList[3];
+	string names[3];
+
+	cout << "3명의 이름을 입력해주세요. (선생님, 학생, 학생)" << endl;
+	cin >> names[0] >> names[1] >> names[2];
+
+	/*names 배열 이용하여 각 class 생성*/
+	Teacher teacher(names[0]);
+	Student student1(names[1]);
+	Student student2(names[2]);
+
+	/*pList에 할당하는 코드 추가*/
+	pList[0] = &teacher;
+	pList[1] = &student1; 
+	pList[2] = &student2;
+
+	for (auto p : pList) {
+		p->intro();
+	}
+
+	/*각 class의 고유 함수 실행 (teach(), learn())*/
+	((Teacher*)pList[0])->teach();
+	((Student*)pList[1])->learn();
+	((Student*)pList[2])->learn();
+
+
+	return 0;
+}
